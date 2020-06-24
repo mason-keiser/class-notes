dc6726e3b532fe2a1fb44175d189c44ff0067636
John Nguyen <jnguyen92780@gmail.com>
2020-06-24 13:18:17 -0700

    modified link path in note.jsx to get that specific note

--- client/components/note.jsx
+++ client/components/note.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function NoteItem(props) {
   const note = props.note;
+  const noteId = note.noteId;
   return (
     <div id={note.noteId}>
       <div>
-        <Link to="/notes/{note.noteId}">
+        <Link to={{
+          pathname: '/api/notes/' + noteId
+        }}>
           <h3>{note.noteTitle}</h3>
         </Link>
         <p>{note.noteContent}</p>

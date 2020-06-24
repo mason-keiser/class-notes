b891fd672856fa002e11548023c34d59ce92fcab
John Nguyen <jnguyen92780@gmail.com>
2020-06-24 14:52:25 -0700

    fixing link for notebook.jsx

--- client/components/notebook.jsx
+++ client/components/notebook.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import NoteList from './note-list';
 import Header from './header';
 
-export default class Notebook extends React.Component {
+export default class Notebooks extends React.Component {
   render() {
     return (
       <div>

183dfc81cea70c56b5089881055129fe1a0bc7a7
John Nguyen <jnguyen92780@gmail.com>
2020-06-25 19:47:37 -0700

    in ReviewFlashcards component, in its render method, if there are flashcards in state, then it will render the Flashcard, Indicator, and NextButton

--- client/components/review-flashcards.jsx
+++ client/components/review-flashcards.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-// eslint-disable-next-line no-unused-vars
 function NextButton(props) {
   return (
     <Button>
@@ -14,7 +13,6 @@ function NextButton(props) {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
 function Indicator(props) {
   return (
     <div className={
@@ -28,7 +26,6 @@ function Indicator(props) {
   );
 }
 
-// eslint-disable-next-line no-unused-vars
 function Flashcard(props) {
   return (
     <div className={
@@ -47,7 +44,8 @@ export default class ReviewFlashcards extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      flashcards: []
+      flashcards: [],
+      activeIndex: 0
     };
     this.getFlashcards = this.getFlashcards.bind(this);
   }
@@ -64,83 +62,50 @@ export default class ReviewFlashcards extends React.Component {
       .catch(err => console.error('getFlashcards() fetch failed:', err));
   }
 
+  goToNextFlashcard() {
+    let index = this.state.activeIndex;
+    const length = this.state.flashcards.length;
+    if (index === length - 1) {
+      index = 0;
+    } else {
+      index++;
+    }
+    this.setState({
+      activeIndex: index
+    });
+  }
+
   componentDidMount() {
     this.getFlashcards();
   }
 
+  render() {
+    if (this.state.flashcards === null) {
+      return null;
+    }
+    return (
+      <div className="flashcard-review-container col-12">
+        <div className="flashcard-container col-10 offset-1 d-flex align-items-center justify-content-center">
+          {this.state.flashcards.map((flashcard, index) =>
+            <Flashcard
+              key={index}
+              index={index}
+              activeIndex={this.state.activeIndex}
+              flashcard={flashcard} />
+          )}
+          {this.state.flashcards.map((flashcard, index) =>
+            <Indicator
+              key={index}
+              index={index}
+              activeIndex={this.state.activeIndex}
+              flashcard={flashcard} />
+          // onClick={event => this.showAnswer()} />
+          // in above line, when flashcard is clicked, if question is showing, show answer.  if answer is showing, show next card
+          )}
+          <NextButton
+            goToNextFlashcard={() => this.goToNextFlashcard()} />
+        </div>
+      </div>
+    );
+  }
 }
-
-// export default class NoteList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       notes: [],
-//       notebookName: []
-//     };
-//     this.getNotes = this.getNotes.bind(this);
-//     this.getNotebookName = this.getNotebookName.bind(this);
-//   }
-
-//   getNotes() {
-//     fetch('api/notes')
-//       .then(response => response.json())
-//       .then(notesData => {
-//         this.setState({
-//           notes: notesData
-//         });
-//       })
-//       .catch(err => console.error('getNotes() fetch failed:', err));
-//   }
-
-//   getNotebookName() {
-//     fetch('api/notebooks/1')
-//       .then(response => response.json())
-//       .then(notebookData => {
-//         this.setState({
-//           notebookName: notebookData[0].notebookName
-//         });
-//       })
-//       .catch(err => console.error('getNotebookName() fetch failed:', err));
-//   }
-
-//   componentDidMount() {
-//     this.getNotes();
-//     this.getNotebookName();
-//   }
-
-//   render() {
-//     return (
-//       <div className="note-list-container d-flex justify-content-center">
-//         <div className="d-flex flex-wrap card-deck note-list-container-border">
-//           <h1 className="col-12 text-center notebook-name mb-5 mt-2">{this.state.notebookName}</h1>
-//           {this.state.notes.map(noteListItem => {
-//             return (
-//               <NoteListItem
-//                 key={noteListItem.noteId}
-//                 note={noteListItem} />
-//             );
-//           })}
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// function NoteListItem(props) {
-//   const noteListItem = props.note;
-//   const noteId = noteListItem.noteId;
-//   return (
-//     <div className="my-y pb-3 col-4 note-list-item" id={noteId}>
-//       <div className="card h-100">
-//         <div className="card-body m-0 p-0 note-list-item-body rounded-0 h-100">
-//           <Link to={{
-//             pathname: '/notes/' + noteId
-//           }}>
-//             <p className="card-title text-left note-list-item-title">{noteListItem.noteTitle}</p>
-//             <p className="card-text text-left note-list-item-content h-100 mb-3">{noteListItem.noteContent}</p>
-//           </Link>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }

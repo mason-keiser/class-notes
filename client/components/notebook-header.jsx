fda00569d4063c3f6c195c0e900b227866eec5c8
Jingwen Wu <wuesperanza@gmail.com>
2020-06-28 16:35:22 -0700

    style update for header and no note page

--- client/components/notebook-header.jsx
+++ client/components/notebook-header.jsx
@@ -5,21 +5,21 @@ import { Link } from 'react-router-dom';
 export default class NotebookHeader extends React.Component {
   render() {
     return (
-      <header className="header-container">
-        <div className="header-left-container">
+      <header className="header-container d-flex flex-row justify-content-between">
+        <div className="col-4">
           <Link to="/">
             <i className="fa fa-bars theme-green fa-2x header-hamburger-icon"></i>
           </Link>
         </div>
-        <div className="header-center-container">
+        <div className="d-flex flex-row justify-content-center col-4">
           <Link to="/notes/create">
             <Button outline color="success" className="header-outline-button">New Note</Button>
           </Link>
         </div>
-        <div className="header-right-container">
-          <div className="header-search-block">
+        <div className="d-flex flex-row align-items-center justify-content-end col-4">
+          <div className="search-block mr-5">
             <h5>Search</h5>
-            <div className="header-search-deco"></div>
+            <div className="search-deco"></div>
           </div>
           <p className="theme-green header-student-name">{this.props.studentName}</p>
         </div>

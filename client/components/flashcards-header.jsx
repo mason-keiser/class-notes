f12b0bfb8df41e35eb9d50728a599118115759b1
Mason Keiser <masonksr5@gmail.com>
2020-07-05 22:02:54 +0000

    search available on fc page

--- client/components/flashcards-header.jsx
+++ client/components/flashcards-header.jsx
@@ -1,30 +1,56 @@
 import React from 'react';
 import { Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import SearchBar from './search-bar';
 
 export default class FlashcardsHeader extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchOpened: false
+    }
+    this.searchClicked = this.searchClicked.bind(this)
+    this.closeXClicked = this.closeXClicked.bind(this)
+  }
+
+  searchClicked() {
+    this.setState({
+      searchOpened: true
+    })
+  }
+
+  closeXClicked() {
+    this.setState({
+      searchOpened: false
+    })
+  }
+
+
   render() {
     return (
-      <header className="header-container d-flex justify-content-between">
-        <div className="col-4">
-          <Link to="/">
-            <img src="/images/code-note-icon.png" alt="Code Note Icon" />
-            {/* <i className="fa fa-bars theme-green fa-2x header-hamburger-icon"></i> */}
-          </Link>
-        </div>
-        <div className="d-flex flex-row justify-content-center col-4">
-          <Link to="/notebook">
-            <Button outline color="success" className="header-outline-button">Notebook</Button>
-          </Link>
-        </div>
-        <div className="d-flex flex-row align-items-center justify-content-end col-4">
-          <div className="search-block mr-5">
-            <h5>Search</h5>
-            <div className="search-deco"></div>
+      <div>
+        <SearchBar searchClicked={this.searchClicked} closeXClicked={this.closeXClicked} isOpened={this.state.searchOpened}/>
+        <header className="header-container d-flex justify-content-between">
+          <div className="col-4">
+            <Link to="/">
+              <img src="/images/code-note-icon.png" alt="Code Note Icon" />
+              {/* <i className="fa fa-bars theme-green fa-2x header-hamburger-icon"></i> */}
+            </Link>
           </div>
-          <p className="theme-green header-student-name">{this.props.studentName}</p>
-        </div>
-      </header>
+          <div className="d-flex flex-row justify-content-center col-4">
+            <Link to="/notebook">
+              <Button outline color="success" className="header-outline-button">Notebook</Button>
+            </Link>
+          </div>
+          <div className="d-flex flex-row align-items-center justify-content-end col-4">
+            <div className="search-block mr-5" onClick={this.searchClicked}>
+              <h5>Search</h5>
+              <div className="search-deco"></div>
+            </div>
+            <p className="theme-green header-student-name">{this.props.studentName}</p>
+          </div>
+        </header>
+      </div>
     );
   }
 }
